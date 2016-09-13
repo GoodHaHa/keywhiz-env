f116283100c732ddf1249c9f1ec6751b0c095edb
Gyula Weber <gyula.weber.in@gmail.com>
2016-09-13 13:37:18 +0200

    added tool building

--- workflow.sh
+++ workflow.sh
@@ -8,3 +8,22 @@ set -o pipefail
 cat repo_list.txt | grep -v '^$' | while read LN; do
   ./ensure_repo.sh "${LN}"
 done
+
+### build certstrap
+echo "--> building certstrap"
+( cd certstrap;
+  ./build
+)
+
+### build keywhiz
+echo "--> building keywhiz"
+( cd keywhiz;
+  docker build --rm --force-rm -t square/keywhiz .
+)
+
+### build keywhiz-fs
+
+echo "--> building keywhiz-fs"
+( cd keywhiz-fs;
+  docker build --rm -t square/keywhiz-fs .
+)

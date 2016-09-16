0e51d4e785551f4dfe34c90eb787a71162e60055
Gyula Weber <gyula.weber.in@gmail.com>
2016-09-16 14:26:49 +0200

    cookie key path is actually a cookie key... damn...

--- generate-docker-config.sh
+++ generate-docker-config.sh
@@ -9,15 +9,17 @@ echo "--> ${0} starting"
 export KEYWHIZ_CONFIG='certstrap/out/keywhiz-docker.yaml'
 export KEYSTORE_PATH='/secrets/keywhiz-server.p12'
 
-export KEYSTORE_PASSWORD=""
+export KEYSTORE_PASSWORD=''
 #KEYSTORE_PASSWORD=$(cat certstrap/out/content_keystore_password)
 
 export TRUSTSTORE_PATH='/secrets/ca-bundle.p12'
 export TRUSTSTORE_PASSWORD='ponies'
 export CRL_PATH='/secrets/ca-crl.pem'
-export COOKIE_KEY_PATH='/secrets/cookie.key.base64'
+export COOKIE_KEY_PATH=$(cat certstrap/out/cookie.key.base64)
 export CONTENT_KEYSTORE_PATH='/secrets/content-encryption-key.jceks'
 export CONTENT_KEYSTORE_PASSWORD=$(cat certstrap/out/content_keystore_password)
+
+echo "cookie_key_path: ${COOKIE_KEY_PATH}"
 
 echo "--> generating: ${KEYWHIZ_CONFIG}"
 envsubst < config/keywhiz-config.tpl > ${KEYWHIZ_CONFIG}

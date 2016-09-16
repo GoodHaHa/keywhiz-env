a8696cad713894cf99d9781a679c25954ce69905
Gyula Weber <gyula.weber.in@gmail.com>
2016-09-16 11:54:28 +0200

    update stuff

--- generate_new_certs.sh
+++ generate_new_certs.sh
@@ -4,17 +4,22 @@ set -u
 set -e
 set -o pipefail
 
-DRUN="docker run -ti --rm -v $(pwd)/certstrap:/srv -w /srv java8"
+echo "--> ${0} started"
+
 CDIR=$(pwd)
+DRUN="docker run -ti --rm -v $(pwd)/certstrap:/srv -w /srv java8"
+DCP="docker run --rm -v keywhiz-secrets:/secrets -v $(pwd):/srv ubuntu:trusty"
 
 echo "--> cleaning any generated certificate."
 ( cd certstrap;
   sudo rm -rf out/*
 )
 
+(./create_cookie_key.sh)
+(./create_content_keystore_password.sh)
+
 echo "--> creating CA";
 ( cd certstrap;
-  $DRUN ls
   $DRUN bin/certstrap init --key-bits 4096 --years 5 --common-name "Keywhiz CA";
   $DRUN keytool -import -file 'out/Keywhiz_CA.crt' -alias ca -storetype pkcs12 -storepass ponies -keystore out/Keywhiz_CA.p12;
   sudo cp out/Keywhiz_CA.p12 out/truststore.p12
@@ -23,12 +28,7 @@ echo "--> creating CA";
 echo "--> creating client certificates";
 ( cd certstrap;
   $DRUN bin/certstrap request-cert --common-name client;
-  echo "--- listing files ---";
-  $DRUN ls
-  echo "---------------------";
   $DRUN bin/certstrap sign --years 1 --CA "Keywhiz CA" client;
-  $DRUN bin/certstrap request-cert --common-name noSecretsClient;
-  $DRUN bin/certstrap sign --years 1 --CA "Keywhiz CA" noSecretsClient
 )
 
 echo "--> creating server certificate"
@@ -42,19 +42,8 @@ echo "--> creating server certificate"
   sudo chmod 744 ${CDIR}/certstrap/out/client.key
 
   openssl rsa -in ${CDIR}/certstrap/out/client.key -out ${CDIR}/certstrap/out/client.unencrypted.key -passin pass:ponies
-  openssl rsa -in ${CDIR}/certstrap/out/localhost.key -out ${CDIR}/certstrap/out/localhost.unencrypted.key -passin pass:ponies
 
-  cat ${CDIR}/certstrap/out/localhost.crt ${CDIR}/certstrap/out/Keywhiz_CA.crt >${CDIR}/certstrap/out/cert_chain.pem;
   cat ${CDIR}/certstrap/out/client.crt ${CDIR}/certstrap/out/client.unencrypted.key >${CDIR}/certstrap/out/client.pem
-
-
-echo "--> generating pem files"
-cd certstrap
-sudo chown -R webi.webi ${CDIR}/certstrap/out
-cat ${CDIR}/certstrap/out/localhost.crt ${CDIR}/certstrap/out/localhost.key >${CDIR}/certstrap/out/localhost.pem
-cat ${CDIR}/certstrap/out/Keywhiz_CA.crt ${CDIR}/certstrap/out/Keywhiz_CA.key >${CDIR}/certstrap/out/Keywhiz_CA.pem
-
-cd ..
 
 
 ### remove volumes
@@ -67,28 +56,14 @@ docker volume create --name keywhiz-secrets
 
 ### copy files to the volume
 echo "--> copying files to the volume"
-DCP="docker run --rm -v keywhiz-secrets:/data -v $(pwd):/srv ubuntu:trusty"
 echo "----- srv ----"
 $DCP ls /srv/certstrap/out
 echo "----- data ----"
-$DCP ls /data
-echo "----- copying -----"
+ls /srv
+echo "----- copying ----"
+$DCP cp /srv/certstrap/out/Keywhiz_CA.p12 /secrets/ca-bundle.p12
+$DCP cp /srv/certstrap/out/localhost.p12 /secrets/keywhiz-server.p12
+$DCP cp /srv/certstrap/out/cookie.key.base64 /secrets/cookie.key.base64
+$DCP cp /srv/certstrap/out/content-encryption-keys.jceks /secrets/content-encryption-keys.jceks
 
-# keystore from PRIVATE_KEY_PEM and CERT_CHAIN_PEM
-
-# CERT_CHAIN_PEM
-$DCP cp /srv/certstrap/out/cert_chain.pem /data/keywhiz.pem
-
-# PRIVATE_KEY_PEM
-sudo chmod 644 certstrap/out/*
-$DCP cp /srv/certstrap/out/localhost.unencrypted.key /data/keywhiz-key.pem
-
-# CA_BUNDLE_PEM
-cp ${CDIR}/certstrap/out/client.crt ${CDIR}/certstrap/out/localhost.crt >${CDIR}/certstrap/out/bundle.pem
-$DCP cp /srv/certstrap/out/localhost.crt /data/ca-bundle.pem
-
-# Certification revocation something
-touch ${CDIR}/certstrap/out/nothing.crl
-$DCP cp /srv/certstrap/out/nothing.crl /data/ca-crl.pem
-
-echo "start wizard with ./wizard.sh to install certificates"
+echo "--> ${0} finished"

20e5a4a57a071c7f3580e6d42b4e3669c8ff49e8
Gyula Weber <gyula.weber.in@gmail.com>
2016-09-17 12:44:09 +0200

    call it builder

--- everything.sh
+++ everything.sh
@@ -5,28 +5,23 @@ set -u
 set -o pipefail
 
 echo "--> building tmp container"
-cd tmp_container
-docker build -t tmp_keywhiz_stuff .
-cd ..
+( cd builder;
+  docker build -t tmp_keywhiz_stuff .
+)
 
-### ensure repositories are in place ###
 cat repo_list.txt | grep -v '^$' | while read LN; do
   ./ensure_repo.sh "${LN}"
 done
 
-### build certstrap
-echo "--> building certstrap"
+echo "--> building certstrap with golang"
 ( cd certstrap;
   docker run -ti --rm -v $(pwd):/srv -w /srv golang ./build
 )
 
-### build keywhiz
 echo "--> building keywhiz"
 ( cd keywhiz;
   docker build --rm --force-rm -t square/keywhiz .
 )
-
-### build keywhiz-fs
 
 echo "--> building keywhiz-fs"
 ( cd keywhiz-fs;

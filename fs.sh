bd87e5ec9f070e894268bb4a311d9e663df7bb8c
Gyula Weber <gyula.weber.in@gmail.com>
2016-09-17 15:39:44 +0200

    updated to have the current values

--- fs.sh
+++ fs.sh
@@ -4,4 +4,8 @@ set -e
 set -u
 set -o pipefail
 
-(cd certstrap/out; keywhiz-fs --key client.pem --ca Keywhiz_CA.crt https://127.0.0.1:4444 /secret/kwfs)
+. ./.env
+
+cd certstrap/out
+echo keywhiz-fs --key ${CRT_CLIENT_NAME}.pem --ca ${CA_NAME}.crt https://127.0.0.1:4444 /secret/kwfs
+keywhiz-fs --key ${CRT_CLIENT_NAME}.pem --ca ${CA_NAME}.crt https://127.0.0.1:4444 /secret/kwfs

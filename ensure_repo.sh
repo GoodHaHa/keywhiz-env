fe43a8f3523b793ffe82cb8eb13c3aecc7aa123e
Gyula Weber <gyula.weber.in@gmail.com>
2016-09-19 20:23:53 +0200

    use https to avoid messing with ssh host keys

--- ensure_repo.sh
+++ ensure_repo.sh
@@ -7,7 +7,7 @@ set -o pipefail
 REPO_NAME=$(basename "${1}" .git)
 if [[ ! -d "${REPO_NAME}" ]]; then
     echo "--> cloning into ${REPO_NAME}"
-    git clone --depth 1 "${1}"
+    git clone "${1}"
 else
     echo "--> updating ${REPO_NAME}"
     (cd ${REPO_NAME}; git pull)

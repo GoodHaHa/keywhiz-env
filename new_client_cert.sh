71e10225aab45e54803bc4a877953f1051ef0c8a
Gyula Weber <gyula.weber.in@gmail.com>
2016-10-05 21:02:33 +0200

    added CA name

--- new_client_cert.sh
+++ new_client_cert.sh
@@ -8,14 +8,12 @@ client="${1}"
 
 . ./.env
 
-#TODO: use CA name from the env file
-
 echo "--> creating client certificates";
 ( cd certstrap;
   bin/certstrap request-cert --common-name ${client};
-  bin/certstrap sign --years 1 --CA "Keywhiz CA" ${client};
+  bin/certstrap sign --years 1 --CA "${CA_NAME}" ${client};
   bin/certstrap request-cert --common-name noSecrets${client};
-  bin/certstrap sign --years 1 --CA "Keywhiz CA" noSecrets${client}
+  bin/certstrap sign --years 1 --CA "${CA_NAME}" noSecrets${client}
 )
 
 echo "certstrap/out/${client}.key # for ${client}"

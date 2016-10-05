de29066db08bb354fcccdb8ccb9e06c3e7219b25
Gyula Weber <gyula.weber.in@gmail.com>
2016-10-05 20:09:49 +0200

    ease mounting remote keywhizfs

---
+++ fs-remote.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -u
+
+. ./.env
+
+REMOTE_SERVER="${1}"
+MP="/secret/kwfs"
+
+cd certstrap/out
+echo keywhiz-fs --key ${CRT_CLIENT_NAME}.pem --ca ${CA_NAME}.crt https://${REMOTE_SERVER}:4444 ${MP}
+keywhiz-fs --key ${CRT_CLIENT_NAME}.pem --ca ${CA_NAME}.crt https://${REMOTE_SERVER}:4444 ${MP}

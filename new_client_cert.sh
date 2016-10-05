ee29b680e13a3641a8bdb95e9e484e36c010aa12
Gyula Weber <gyula.weber.in@gmail.com>
2016-10-05 21:43:01 +0200

    new client setup works now

--- new_client_cert.sh
+++ new_client_cert.sh
@@ -5,19 +5,47 @@ set -e
 set -o pipefail
 
 client="${1}"
-
+REMOTE_SERVER="${2}"
 . ./.env
+
+# but, truststore password is dynamically generated, so we want to override it with the stored password
+TRUSTSTORE_PASSWORD=$(cat certstrap/out/TRUSTSTORE_password)
 
 echo "--> creating client certificates";
 ( cd certstrap;
   bin/certstrap request-cert --common-name ${client};
-  bin/certstrap sign --years 1 --CA "${CA_NAME}" ${client};
+  sudo bin/certstrap sign --years 1 --CA "${CA_NAME}" ${client};
   bin/certstrap request-cert --common-name noSecrets${client};
-  bin/certstrap sign --years 1 --CA "${CA_NAME}" noSecrets${client}
+  sudo bin/certstrap sign --years 1 --CA "${CA_NAME}" noSecrets${client}
 )
 
 echo "certstrap/out/${client}.key # for ${client}"
 echo "certstrap/out/${client}.crt # for ${client}"
 
-#TODO: stop keywhiz server, get the truststore file, add the key, copy the truststore file back,
-#      or find a better idea of doing that.
+cat certstrap/out/${client}.crt certstrap/out/${client}.key >certstrap/out/${client}.pem
+
+echo "--> switching to remote host"
+docker-machine env ${REMOTE_SERVER} || exit 1
+eval $(docker-machine env ${REMOTE_SERVER})
+
+echo "--> getting remote container"
+REMOTE_CONTAINER=$(docker ps | grep 'keywhiz' | awk '{print $1}')
+
+echo "--> copying generated key"
+docker cp certstrap/out/${client}.crt ${REMOTE_CONTAINER}:/tmp/${client}.crt
+
+echo "--> creating command and copying to container"
+echo '#!/bin/bash'>./tmp_cmd.sh
+echo >>./tmp_cmd.sh
+echo "keytool -import -file \"/tmp/${client}.crt\" -alias ${client} -storetype pkcs12 -storepass ${TRUSTSTORE_PASSWORD} -keystore ${TRUSTSTORE_PATH}" >>./tmp_cmd.sh
+chmod +x ./tmp_cmd.sh
+
+docker cp tmp_cmd.sh ${REMOTE_CONTAINER}:/tmp_cmd.sh
+
+echo "--> executing remote command"
+docker exec ${REMOTE_CONTAINER} /tmp_cmd.sh
+
+echo "--> cleaning up"
+rm -f tmp_cmd.sh
+
+echo "--> done"

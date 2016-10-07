398225376dbf31a9f634e295cded19ac72b39c4d
Gyula Weber <gyula.weber.in@gmail.com>
2016-10-07 19:24:48 +0200

    remote is now started in daemon mode

--- start-remote.sh
+++ start-remote.sh
@@ -5,10 +5,11 @@ set -euo pipefail
 docker-machine env ${1} # it will fail if its incorrect
 eval $(docker-machine env ${1})
 
-docker run -it --rm \
+docker run -d \
     -v keywhiz-data:/data \
     -v keywhiz-secrets:/secrets \
     -p 4444:4444 \
     -e KEYWHIZ_CONFIG=/data/keywhiz-docker.yaml \
+    --name keywhiz_server \
     gyulaweber/keywhiz server
 
